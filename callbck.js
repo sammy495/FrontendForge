85733d57826693c2289f282a1ed19fb505fe6974
sammy495 <sskenvadekar@gmail.com>
2026-06-23 22:09:11 +0530

    solving questions

--- callbck.js
+++ callbck.js
@@ -1,4 +1,4 @@
-function displayMessage(message, callback) {
+/*function displayMessage(message, callback) {
   console.log(message);
   callback();
 }
@@ -6,3 +6,22 @@ function msgcl() {
   console.log("task complete!");
 }
 displayMessage("this is an msg!", msgcl);
+
+**/
+
+//2 ⃣ Create a function calculate(a, b, callback) that performs addition, subtraction, or multiplication
+//depending on which callback is passed. 
+
+function calculate(a, b, callback) {
+  return callback(a, b);
+}
+function add(a, b) {
+  console.log(a + b);
+}
+
+function sub(a, b) {
+  console.log(a - b);
+}
+
+
+calculate(2, 4, add);
